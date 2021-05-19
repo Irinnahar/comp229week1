de4b900506f8530abd4d148ed6336d8a7a803537
Irinnahar <irincse.bu@gmail.com>
2021-05-19 12:26:51 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function() {
+    function start () {
+        let myVariable = 20;
+        console.log("My variable is : " + myVariable);
+    }
+
+    window.addEventListener("load", start);
+}) ();
+
